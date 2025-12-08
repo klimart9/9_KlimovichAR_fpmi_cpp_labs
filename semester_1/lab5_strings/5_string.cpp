f505cd4199a87b0e4977fcc6bc0fd15b9d5475f5
klimart9 <klimart9@gmail.com>
2025-12-08 17:32:38 +0300

    Update 5_string.cpp with correct solution

--- semester_1/lab5_strings/5_string.cpp
+++ semester_1/lab5_strings/5_string.cpp
@@ -3,69 +3,86 @@
 #include <cctype>
 #include <cmath>
 
-int main()
-{
-	std::string str;
-	std::string new_str;
-	std::cout << "Write your string" << std::endl;
-	getline(std::cin, str);
-	if (str == "")
-	{
-		std::cout << "Write not null  string" << std::endl;
-		return 1;
-	}
-	for (int i = 0; i < str.length(); i++)
-	{
-		while (std::isdigit(str[i]))
-		{
-			new_str = new_str + str[i];
-			i = i + 1;
-			if (!(std::isdigit(str[i])))
-			{
-				new_str = new_str + " ";
-			}
-		}
-	}
-	if (new_str == "")
-	{
-		std::cout << "Your sring doesn't contain numbers" << std::endl;
-		return 1;
-	}
-	std::cout << "\nNumbers:" << new_str << std::endl;
-	std::string current_number;
-	std::string answer;
-	for (int i = 0; i < new_str.length(); i++)
-	{
-		while (std::isdigit(new_str[i]))
-		{
-			current_number = current_number + new_str[i];
-			i = i + 1;
-			if (!(std::isdigit(new_str[i])))
-			{
-				bool is_simple = true;
-				int number = std::stoi(current_number);
-				for (int i = 2; i < sqrt(number); i++)
-				{
-					if (number % i == 0)
-					{
-						is_simple = false;
-					}
-				}
-				if (is_simple)
-				{
-					answer = answer + current_number + " ";
-				}
-				current_number = "";
-			}
-		}
-	}
-	if (answer == "")
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
 
-	{
-		std::cout << "String doesn't contain simple numbers" << std::endl;
-		return 1;
-	}
-	std::cout << "\nSimple numbers in your string: " << answer << std::endl;
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
-	return 0;
+bool isDelimiter(char c) {
+    return std::ispunct(c) || std::isspace(c);
+}
+
+bool isAllDigits(const std::string& str) {
+    if (str.empty()) {
+        return false;
+    }
+    for (char c : str) {
+        if (!std::isdigit(c)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string extractWords(const std::string& input) {
+    std::string result;
+    std::string currentWord;
+
+    for (size_t i = 0; i < input.length(); i++) {
+        char c = input[i];
+
+        if (!isDelimiter(c)) {
+            currentWord += c;
+        }
+
+        if (isDelimiter(c) || i == input.length() - 1) {
+            if (!currentWord.empty()) {
+                if (isAllDigits(currentWord)) {
+                        int num = std::stoi(currentWord);
+                        if (isPrime(num)) {
+                            if (!result.empty()) {
+                                result += " ";
+                            }
+                            result += currentWord;
+                        }
+                    }
+                }
+
+                currentWord.clear();
+            }
+        }
+    return result;
+}
+
+
+int main() {
+    try {
+        std::string input;
+        std::cout << "Write your string: " << std::endl;
+        getline(std::cin, input);
+
+        if (input.empty()) {
+            throw "Your string is empty";
+        }
+
+        std::string result = extractWords(input);
+
+        if (result.empty()) {
+            throw "Input string doesn't contain simple numbers";
+        }
+
+        std::cout << "Simple numbers in your string: " << result << std::endl;
+    }
+    catch (const char* msg) {
+        std::cout << msg;
+    }
+    return 0;
 }

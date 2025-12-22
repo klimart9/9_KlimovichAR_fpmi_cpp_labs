177a22bbe124f7fa4cc3607c68de8e1177d37887
klimart9 <klimart9@gmail.com>
2025-12-22 14:32:57 +0300

    splited solution from lab7.cpp for tests

--- semester_1/lab7_class_vector/vector/vector.h
+++ semester_1/lab7_class_vector/vector/vector.h
@@ -1,5 +1,48 @@
 #pragma once
+#include <iostream>
+#include <initializer_list>
+#include <cstddef>
 
 class Vector {
-    // your code goes here
+private:
+    size_t size_ = 0;
+    int* data_ = nullptr;
+    size_t capacity_ = 0;
+
+    
+
+public:
+    Vector();
+
+    Vector(size_t n);
+
+    Vector(std::initializer_list<int> list);
+
+    Vector(const Vector& other);
+
+    Vector& operator=(const Vector& other);
+
+    ~Vector();
+
+    size_t Size() const;
+
+    size_t Capacity() const;
+
+    void Reallocate(size_t new_capacity);
+
+    void PushBack(int elem);
+
+    void PopBack();
+
+    void Clear();
+
+    void Reserve(size_t new_capacity);
+
+    void Swap(Vector& other);
+
+    const int& operator[](size_t index) const;
+
+    int& operator[](size_t index);
 };
+
+std::ostream& operator<<(std::ostream& out, const Vector& vec);

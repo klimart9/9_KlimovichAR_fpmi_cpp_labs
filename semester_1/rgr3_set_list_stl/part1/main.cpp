5e30c73fd03c42b6e6765bccec036eacb220cdcc
klimart9 <klimart9@gmail.com>
2025-12-25 21:20:04 +0300

    Update main.cpp with completed part 1

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,39 @@
-
-
+#include <iostream>
+#include <set>
 
 int main() {
+    try {
+        int n;
+        if (!(std::cin >> n)) {
+            throw "Enter the number";
+        }
+        if (n < 2) {
+            throw "Enter the number > 1";
+        }
+        if (n > 45000) {
+            throw "Cant use the algorithm for those numbers. Enter number lower than 45000";
 
-    // your code goes here
+        }
+
+        std::set<int> primes;
+        for (int i = 2; i < n; ++i) {
+            primes.insert(i);
+        }
+
+        for (std::set<int>::iterator it = primes.begin(); it != primes.end(); ++it) {
+            int p = *it;
+            for (int j = p * p; j < n; j += p) {
+                primes.erase(j);
+            }
+        }
+
+        for (std::set<int>::iterator it = primes.begin(); it != primes.end(); ++it) {
+            std::cout << *it << " ";
+        }
+    }
+    catch (const char* msg) {
+        std::cout << msg;
+    }
 
     return 0;
 }
